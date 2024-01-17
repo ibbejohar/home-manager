6eff7f9f39a518ec0172067a21711dab91ec0e9a
Ibrahim Johar <ibbe.johar@gmail.com>
2024-01-17 18:13:55 +0100

    added more config

--- config/ncmpcpp/ncmpcpp.nix
+++ config/ncmpcpp/ncmpcpp.nix
@@ -6,6 +6,20 @@
     ncmpcpp = {
       enable = true;
       mpdMusicDir = "/media/ColdStorage/music";
+      settings = {
+        mpd_host = "localhost";
+        mpd_port = "6600";
+        mpd_music_dir = "/media/ColdStorage/music/";
+
+        lyrics_directory = "~/.config/ncmpcpp/";
+        ncmpcpp_directory = "~/.config/ncmpcpp/";
+      };
+      bindings = [  
+      { key = "j"; command = "scroll_down"; }  
+      { key = "k"; command = "scroll_up"; }  
+      { key = "J"; command = [ "select_item" "scroll_down" ]; }  
+      { key = "K"; command = [ "select_item" "scroll_up" ]; }
+      ];
     };
   };
 
